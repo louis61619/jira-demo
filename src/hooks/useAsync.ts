cea4f8a21947345be93d2f42b858ce8e46b1660a
louis61619 <louis61619@gmail.com>
2021-11-01 19:36:44 +0800

    feat(hook): change useAsync with useCallback

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useReducer } from 'react'
 import { useMountedRef } from './useMount'
 
 interface AsyncState<D> {
@@ -17,37 +17,60 @@ const defaultConfig = {
   throwOnError: false
 }
 
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+  const { mountedRef } = useMountedRef()
+
+  return useCallback(
+    (...args: T[]) => {
+      mountedRef.current ? dispatch(...args) : void 0
+    },
+    [mountedRef, dispatch]
+  )
+}
+
 export const useAsync = <D>(initialState?: AsyncState<D>, initialConfig?: typeof defaultConfig) => {
   const config = {
     ...defaultConfig,
     ...initialConfig
   }
-  const [state, setstate] = useState({
-    ...defaultInitialState,
-    ...initialState
-  })
+  // const [state, setstate] = useState({
+  //   ...defaultInitialState,
+  //   ...initialState
+  // })
+  // 使用 useReducer 自動將新的state和舊的state進行合併
+  const [state, dispatch] = useReducer(
+    (state: AsyncState<D>, action: Partial<AsyncState<D>>) => ({ ...state, ...action }),
+    {
+      ...defaultInitialState,
+      ...initialState
+    }
+  )
 
-  const { mountedRef } = useMountedRef()
+  // const { mountedRef } = useMountedRef()
+  const safeDispatch = useSafeDispatch(dispatch)
 
   // useState會進行一個惰性初始化的操作，所以要保存兩層
   const [retry, setRetry] = useState(() => () => {})
 
   const setData = useCallback(
     (data: D) =>
-      setstate({
+      safeDispatch({
         data,
         stat: 'success',
         error: null
       }),
-    []
+    [safeDispatch]
   )
 
-  const setError = (error: Error) =>
-    setstate({
-      error,
-      stat: 'error',
-      data: null
-    })
+  const setError = useCallback(
+    (error: Error) =>
+      safeDispatch({
+        error,
+        stat: 'error',
+        data: null
+      }),
+    [safeDispatch]
+  )
 
   const run = useCallback(
     (promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
@@ -62,12 +85,11 @@ export const useAsync = <D>(initialState?: AsyncState<D>, initialConfig?: typeof
       })
 
       // 如果在useCallback中使用到state會導致無限循環
-      setstate((preState) => ({ ...preState, stat: 'loading' }))
+      // setstate((preState) => ({ ...preState, stat: 'loading' }))
+      safeDispatch({ stat: 'loading' })
       return promise
         .then((data) => {
-          if (mountedRef.current) {
-            setData(data)
-          }
+          setData(data)
           return data
         })
         .catch((err) => {
@@ -78,7 +100,7 @@ export const useAsync = <D>(initialState?: AsyncState<D>, initialConfig?: typeof
           return err
         })
     },
-    [config.throwOnError, mountedRef, setData]
+    [config.throwOnError, safeDispatch, setError, setData]
   )
 
   return {
