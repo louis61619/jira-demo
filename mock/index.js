6f1f8c54b240c5fe45c16b7812355ee695ab6fa8
louis <louis61619@gmail.com>
2021-11-18 13:45:42 +0800

    chore(mack): change url

--- mock/index.js
+++ mock/index.js
@@ -1,12 +1,14 @@
 import { setupWorker } from 'msw'
 import { handlers } from './handlers'
 
+const url = process.env.NODE_ENV === 'production' ? '/mockServiceWorker.js' : '/jira-demo/mockServiceWorker.js'
+
 export const startServer = () => {
   const server = setupWorker(...handlers)
   server.start({
     quiet: true,
     serviceWorker: {
-      url: '/mockServiceWorker.js'
+      url
     }
   })
 }
