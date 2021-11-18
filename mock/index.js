cbe25af08fa24a001e26339f139360bf2591fc32
louis <louis61619@gmail.com>
2021-11-18 15:25:47 +0800

    chore(CI): change github action to vercel app

--- mock/index.js
+++ mock/index.js
@@ -1,14 +1,30 @@
 import { setupWorker } from 'msw'
+
 import { handlers } from './handlers'
 
-const url = process.env.NODE_ENV === 'production' ? '/mockServiceWorker.js' : '/jira-demo/mockServiceWorker.js'
+// const url =
+//   process.env.NODE_ENV === 'production'
+//     ? '/mockServiceWorker.js'
+//     : '/jira-demo/mockServiceWorker.js'
+
+// export const handlers = [
+//   // Handles a POST /login request
+//   // rest.post('/login', null),
+//   // Handles a GET /user request
+//   rest.post(`http://192.168.50.181:3000/jira-demo/login`, async (req, res, ctx) => {
+//     // const { username, password } = req.body
+//     const user = { name: 'fkewifjwo', password: 'fjweiofjwio' }
+//     return res(ctx.json({ user }))
+//   })
+// ]
 
 export const startServer = () => {
   const server = setupWorker(...handlers)
   server.start({
     quiet: true,
     serviceWorker: {
-      url
+      url: '/mockServiceWorker.js',
+      scope: '/'
     }
   })
 }
